857fd800d1dc2abe26838e34ef4ab85b5789ed24
pdumrauf <dumraufp@gmail.com>
2024-12-14 19:39:06 +0100

    check winner + refacto

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -9,39 +9,71 @@ const boardGame = [
   [null, null, null],
 ]
 
+  // board[x][y]
+  // board[0][0] => 1 square
+  // board[0][1] => 2 square
+
 const TURNS = {
   X: 'X',
   O: 'O',
 }
 
+const WINNING_COMBINATIONS = [
+  // vertical
+  [[0,0], [1,0], [2,0]],
+  [[0,1], [1,1], [2,1]],
+  [[0,2], [1,2], [2,2]],
+  // horizontal
+  [[0,0], [0,1], [0,2]],
+  [[1,0], [1,1], [1,2]],
+  [[2,0], [2,1], [2,2]],
+  // diagonal
+  [[0,0], [1,1], [2,2]],
+  [[0,2], [1,1], [2,0]],
+]
+
 function App() {
   const [board, setBoard] = useState(boardGame)
   const [currentTurn, setCurrentTurn] = useState(TURNS.X)
-
-  // board[x][y]
-  // board[0][0] => 1 square
-  // board[0][1] => 2 square
-
+  const [winner, setWinner] = useState(null)
 
   const handleSquareClick = (row, col) => {
-    if(board[row][col] !== null) return
-    
-    let newBoard = [...boardGame]
-    for(let i = 0; i < newBoard.length; i++) {
-      let newRow = newBoard[i]
-      for(let j = 0; j < newBoard[i].length; j++) {
-        if(i === row && j === col) {
-          newRow[j] = currentTurn
-        }
-      }
-      newBoard[i] = newRow
-    }
-    
-    console.log('newboard', newBoard)
-    console.log('board', board)
-    console.log('currentTurn', currentTurn)
+    // no update if there's already X, O or winner
+    if (board[row][col] || winner) return
+  
+    // update board
+    const newBoard = board.map(row => [...row])
+    newBoard[row][col] = currentTurn
+    console.log('newBoard', newBoard)
     setBoard(newBoard)
-    setCurrentTurn(currentTurn === TURNS.X ? TURNS.O : TURNS.X)
+
+    // change turn
+    const newTurn= currentTurn === TURNS.X ? TURNS.O : TURNS.X
+    setCurrentTurn(newTurn)
+  
+    //  check winner
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner);
+      console.log('newWinner',newWinner)
+    }
+  }
+  
+
+  const checkWinner = (board) => {
+    // check all winning combinations
+    for(let combination of WINNING_COMBINATIONS) {
+      const [a, b, c] = combination
+
+      if( board[a[0]][a[1]] && 
+          board[a[0]][a[1]] === board[b[0]][b[1]] && 
+          board[a[0]][a[1]] === board[c[0]][c[1]] 
+      ) {
+        return board[a[0]][a[1]] // X or O
+      }
+
+      return null // no winner
+    }
   }
 
   return (
