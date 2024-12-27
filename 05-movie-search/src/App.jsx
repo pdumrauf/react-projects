3b166fad1a3d1bf33ecd097d4efe74e1a5d1eee6
pdumrauf <dumraufp@gmail.com>
2024-12-27 20:29:17 +0100

    add api and component for movies

--- 05-movie-search/src/App.jsx
+++ 05-movie-search/src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import withResults from './mocks/with-results.json'
+// import withoutResults from '../mocks/no-results.json'
+import { Movies } from './components/Movies'
 
 function App() {
+  const movies = withResults.Search
 
   return (
     <>
+      <header>
+        <h1>Movie Search</h1>
+        <form className='form'>
+          <input placeholder='Star Wars, Harry Potter...'></input>
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+
+      <main>
+        <Movies movies={movies} />
+      </main>
     </>
   )
 }
