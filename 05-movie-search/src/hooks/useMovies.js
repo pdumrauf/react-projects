50b9c50184ac26346ad884cd1283fcc52dc75fb4
pdumrauf <dumraufp@gmail.com>
2025-01-12 17:42:33 +0100

    avoid multiple api calls for same search

--- 05-movie-search/src/hooks/useMovies.js
+++ 05-movie-search/src/hooks/useMovies.js
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
+  const previousSearch = useRef(search)
 
   async function getMovies () {
+  if(search === previousSearch.current) return
+
   try {
     setLoading(true)
+    previousSearch.current = search
     const newMovies = await searchMovies({ search })
     setMovies(newMovies)
   } catch (error) {
